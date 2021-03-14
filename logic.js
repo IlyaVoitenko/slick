596f70014510a44c9d2197e3b38181772c2589a6
ilya <you@example.com>
2021-03-15 01:05:26 +0200

    test slick

---
+++ logic.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    
+  });
+  $('.single-item').slick({
+      arrows:true,
+      fade:true
+  });
+  $('.slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 500,
+    fade: true,
+  });
+  
